88d2967f89a56bf39c957296a79ff4963adbcf2d
Sagar Vats <vats0726@gmail.com>
2019-04-03 20:43:17 +0530

    Questions on Basic Concepts
    This Folder will contain the Implementation of Basic concepts of Data-stuctures and Algorithms

---
+++ Basics/Topological_sort.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Graph
+{
+    int N;
+
+public :
+        Graph(int N);
+        void addedge(int u,int v);
+        void display()
+        
+
+}
+
+Graph(int N)
+{
+    this->N=N;
+    
+}
+int main()
+{
+    list<int> l;
+    l.insert(10);
+   
+    l.pop_front();
+    
+
+}
