0ab090842f5db5aaac2386a4e9560b924346e483
Sagar Vats <vats0726@gmail.com>
2019-04-03 22:06:53 +0530

    Updating
    Updating my work

--- Basics/Topological_sort.cpp
+++ Basics/Topological_sort.cpp
@@ -3,27 +3,54 @@ using namespace std;
 
 class Graph
 {
-    int N;
-
+         int N;
+         vector<int> *v;
 public :
-        Graph(int N);
-        void addedge(int u,int v);
-        void display()
+        Graph(int N)
+        {
+            this->N=N;
+            v = new vector<int>[N];         // its similar to java like when we create an array
+        }
+
+        void addedge(int a,int b);
+        void display();
         
 
+};
+
+void Graph::addedge(int a,int b)
+{
+    v[a].push_back(b);
 }
 
-Graph(int N)
+void Graph::display()
 {
-    this->N=N;
-    
+    for(int i=0;i<N;i++)
+    {
+        cout<<"["<<i<<"]"<<":";
+        int sz=v[i].size();
+        for(int j=0;j<sz;j++)
+        {
+            cout<<v[i][j]<<"->";
+        }
+        cout<<endl;
+    }    
 }
+
 int main()
 {
-    list<int> l;
-    l.insert(10);
-   
-    l.pop_front();
-    
+    int n,e;
+    cout<<"\n Enter no. of nodes in a graph";
+    cin>>n;
+    Graph G(n);
+    cout<<"\n Enter no. of edges";
+    cin>>e;
+    for(int i=0;i<e;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        G.addedge(a,b);
+    }
+    G.display();
 
 }
